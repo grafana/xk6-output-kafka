d2d848c5db556ee666bbe22a1dd5f467116be84b
Bruno Barin <bruno.barin@ifood.com.br>
2022-02-17 16:09:47 -0300

    Added profiler

--- register.go
+++ register.go
@@ -1,6 +1,10 @@
 package kafka
 
 import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
 	"github.com/grafana/xk6-output-kafka/pkg/kafka"
 	"go.k6.io/k6/output"
 )
@@ -9,4 +13,5 @@ func init() {
 	output.RegisterExtension("xk6-kafka", func(p output.Params) (output.Output, error) {
 		return kafka.New(p)
 	})
+	log.Println(http.ListenAndServe("localhost:6060", nil))
 }
